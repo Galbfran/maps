c02a71e61dd55a75b59f0280be0a9caa2e384bd3
Galbfran <francogalbiati984@gmail.com>
2024-01-09 14:12:20 -0500

    agrego google web servise

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:flutter_google_places/flutter_google_places.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:google_maps_webservice/places.dart';
 
 void main() {
   runApp(const MyApp());
@@ -31,6 +33,34 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   LatLng? selectedLocation;
+  final places = GoogleMapsPlaces(
+      apiKey:
+          "AIzaSyAA6KXYXkm6KJ84V1apLQguQKXBoKx0NtE"); // Reemplaza con tu clave API
+
+  Future<void> _buscarDireccion() async {
+    Prediction? p = await PlacesAutocomplete.show(
+      context: context,
+      apiKey:
+          "AIzaSyAA6KXYXkm6KJ84V1apLQguQKXBoKx0NtE", // Tu clave API de Google Cloud
+      mode: Mode.overlay, // Modo de búsqueda
+      language: "es", // Idioma
+      components: [
+        Component(Component.country, "pe")
+      ], // Configura según necesidad
+    );
+
+    if (p != null) {
+      PlacesDetailsResponse detail =
+          await places.getDetailsByPlaceId(p.placeId!);
+      final lat = detail.result.geometry!.location.lat;
+      final lng = detail.result.geometry!.location.lng;
+
+      setState(() {
+        selectedLocation = LatLng(lat, lng);
+        // Actualizar el mapa si es necesario
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -42,13 +72,19 @@ class _HomePageState extends State<HomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
+            ElevatedButton(
+              onPressed: _buscarDireccion,
+              child: const Text('Buscar Dirección'),
+            ),
             if (selectedLocation != null)
               Text(
                   'Ubicación Seleccionada: ${selectedLocation!.latitude}, ${selectedLocation!.longitude}'),
             ElevatedButton(
               onPressed: () async {
                 final result = await Navigator.of(context).push<LatLng>(
-                  MaterialPageRoute(builder: (context) => const MapSample()),
+                  MaterialPageRoute(
+                      builder: (context) =>
+                          MapSample(initialLocation: selectedLocation)),
                 );
                 if (result != null) {
                   setState(() {
@@ -66,7 +102,9 @@ class _HomePageState extends State<HomePage> {
 }
 
 class MapSample extends StatefulWidget {
-  const MapSample({super.key});
+  final LatLng? initialLocation;
+
+  const MapSample({super.key, this.initialLocation});
 
   @override
   State<MapSample> createState() => MapSampleState();
@@ -78,26 +116,34 @@ class MapSampleState extends State<MapSample> {
   Set<Marker> _markers = {};
   LatLng? selectedLocation;
 
-  static const CameraPosition _kGooglePlex = CameraPosition(
-    target: LatLng(-12.04967738829701, -77.09668506723912),
-    zoom: 14.4746,
-  );
+  @override
+  void initState() {
+    super.initState();
+    if (widget.initialLocation != null) {
+      selectedLocation = widget.initialLocation;
+      _addMarker(selectedLocation!);
+    }
+  }
+
+  void _addMarker(LatLng latLng) {
+    _markers.add(Marker(
+      markerId: MarkerId(latLng.toString()),
+      position: latLng,
+      infoWindow: InfoWindow(
+          title: 'Ubicación Seleccionada',
+          snippet: '${latLng.latitude}, ${latLng.longitude}'),
+    ));
+  }
 
   void _handleTap(LatLng latLng) {
     setState(() {
       selectedLocation = latLng;
       _markers.clear();
-      _markers.add(Marker(
-        markerId: MarkerId(latLng.toString()),
-        position: latLng,
-        infoWindow: InfoWindow(
-            title: 'Ubicación Seleccionada',
-            snippet: '${latLng.latitude}, ${latLng.longitude}'),
-            
-      ));
-      Navigator.of(context).pop(latLng);
+      _addMarker(latLng);
     });
-    
+
+    // Regresar automáticamente a HomePage con la ubicación seleccionada
+    Navigator.of(context).pop(latLng);
   }
 
   @override
@@ -114,7 +160,11 @@ class MapSampleState extends State<MapSample> {
       ),
       body: GoogleMap(
         mapType: MapType.hybrid,
-        initialCameraPosition: _kGooglePlex,
+        initialCameraPosition: CameraPosition(
+          target: widget.initialLocation ??
+              LatLng(-12.04967738829701, -77.09668506723912),
+          zoom: 14.4746,
+        ),
         onTap: _handleTap,
         markers: _markers,
         onMapCreated: (GoogleMapController controller) {
